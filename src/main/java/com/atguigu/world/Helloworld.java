40e774cb733814c156ee2762bcea29080c218914
czd <596860296@qq.com>
2021-05-12 18:38:27 +0800

    第二章节练习

---
+++ src/main/java/com/atguigu/world/Helloworld.java
@@ -0,0 +1,14 @@
+package com.atguigu.world;
+
+/**
+ * TODO
+ *
+ * @author Robin
+ * @version 1.0
+ * @date 2021/5/12 18:36
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
